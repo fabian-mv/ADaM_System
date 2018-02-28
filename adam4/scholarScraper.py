5b50e60bf619b07bdd3d630a3c21047d0883f1b9
machetazo <fabianmontero1998@gmail.com>
2018-02-28 12:08:35 -0600

    Backup Commit

--- adam4/scholarScraper.py
+++ adam4/scholarScraper.py
@@ -1,6 +1,8 @@
 import scholarly
 import csv
-import re
+from tkinter import *
+from tkinter import ttk
+import urllib
 
 
 def process_row(row):
@@ -25,13 +27,14 @@ def process_row(row):
         return row
 
 
-def build_super_dict():
-    publications = scholarly.search_pubs_query("\"sign languages\" , avatar, translation")
+def build_super_dict(query , path , amount):
+    amount = int(amount)
+    publications = scholarly.search_pubs_query("darwin")
 
     field_names = ["title", "eprint", "author", "abstract", "url"]
 
     counter = 0
-    with open('RESULTADOS.csv', 'w') as file:
+    with open('/home/fabian/Documents/repos/scholar_web_scrapper/results/scrapping_results.csv', 'w') as file:
         csv.register_dialect("toMYSQL", delimiter=";", quoting=1, doublequote=1)
         writer = csv.DictWriter(file, fieldnames=field_names, dialect="toMYSQL")
         writer.writeheader()
@@ -45,15 +48,46 @@ def build_super_dict():
             if clean_row is not None:
                 writer.writerow(clean_row)
 
-            if counter >= 50:
+            if counter >= 15:
                 break
             else:
                 counter = counter + 1
 
 
-def main():
-    build_super_dict()
+def search():
+    path = "/home/fabian/Documents/repos/scholar_web_scrapper/results/"#str(entry_path.get())
+    query = "darwin"#str(entry_query.get())
+    amount = 10#int(entry_amount.get())
+
+    build_super_dict(query , path , amount)
 
 
-if __name__ == "__main__":
-    main()
+
+build_super_dict("" , "" , 10)
+# root = Tk()
+# root.title("Google Scholar Web Scrapper")
+#
+# entry_query = ttk.Entry(root, width=50)
+# entry_query.insert(END, 'darwin')
+# entry_query.grid(column=0, row=1, columnspan=4, padx=7, pady=(0,7))
+#
+# entry_path = ttk.Entry(root, width=30)
+# entry_path.insert(END, '/home/fabian/Documents/repos/scholar_web_scrapper/results')
+# entry_path.grid(column=0, row=3, columnspan=3, padx=7, pady=3 , sticky=W)
+#
+# entry_amount = ttk.Entry(root, width=10)
+# entry_amount.insert(END, '10')
+# entry_amount.grid(column=3, row=3, padx=7, sticky=W)
+#
+# label_search = ttk.Label(root, text="Search:").grid(column=0, row=0, sticky=(W), padx=7)
+# label_path = ttk.Label(root, text="Directory to save results:").grid(column=0, row=2, sticky=(W), padx=7)
+# label_amount = ttk.Label(root, text="Amount of results:").grid(column=3, row=2, sticky=(W), padx=7)
+#
+# button_search = ttk.Button(root, text="Search", command=search).grid(column=3, row=4, sticky=E+W+N+S, padx=7, pady=(3,10))
+#
+# entry_query.focus()
+#
+# root.mainloop()
+
+
+
